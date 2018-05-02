d22329597b0e345828c658776ae093363aa76207
AnnKlimkovich <hklimkovich@exadel.com>
2018-05-02 08:56:52 +0300

    todo in progress

---
+++ ToDoPaper/function.js
@@ -0,0 +1,26 @@
+function addTodoItem(todoItem: object)
+
+//Check if todoItem is valid and if yes, add it to todoItems array. Function should return the result of validity
+//check (text field is not empty, all fields are present and id is unique).
+//todoItem : { text: string, completed: boolean, id: number }
+
+
+function viewTodoList(itemsType: string)
+
+
+//Function takes itemsType argument (‘completed’, ‘not_completed’, ‘all’) and returns all items of this type.
+
+
+function editTodoItem(todoItemId: number, newText: string)
+
+//If newText is not empty, function changes text of todoItem by todoItemId on the new text. It should return
+//flag, whether edit was successful.
+
+
+function deleteTodoItem(todoItemId: number)
+
+//Delete todoItem by todoItemId, return flag, whether delete was successful.
+
+function completeTodoItem(todoItemId: number)
+
+//Change completed field of todoItem (get it by todoItemId) on true.
